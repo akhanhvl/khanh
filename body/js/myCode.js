759a4d61b929e34ee69d1418239cd6066255445e
akhanhvl <152150539+akhanhvl@users.noreply.github.com>
2024-06-12 11:43:39 +0700

    Delete body directory

--- body/js/myCode.js
+++
@@ -1,6 +0,0 @@
-document.querySelector("#popup-open-btn").addEventListener("click", function() {
-    document.querySelector(".popup").classList.add("active");
-});
-document.querySelector(".popup .popup-close-btn").addEventListener("click", function() {
-    document.querySelector(".popup").classList.remove("active");
-});
